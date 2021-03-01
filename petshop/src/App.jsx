594b84aae4828bc0f4fc71c95fc7eb3f7b1a6a4b
TH_Fatecano <thiagosvin@hotmail.com>
2021-03-01 07:12:25 -0300

    Adicionando página de erros e cabecalho

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import "./assets/css/base/base.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import Home from "./paginas/Home";
 import Sobre from "./paginas/Sobre";
+import Pagina404 from "./paginas/Pagina404";
+import Cabecalho from "./components/cabecalho";
 
 function App() {
-  const Router = () => {
-    const location = window.location.pathname;
-    if (location === "/sobre") {
-      return <Sobre />;
-    } else {
-      return <Home />;
-    }
-  };
-  return <>{Router()}</>;
+  return (
+    <Router>
+      <Cabecalho/>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/Sobre">
+          <Sobre />
+        </Route>
+        <Route>
+          <Pagina404 />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
